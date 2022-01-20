522e737d31d0bebc6549152345b66645bcb12315
Ab007007 <aru03.info@gmail.com>
2022-01-20 22:56:03 +0530

    test

--- regressionTest.bat
+++ regressionTest.bat
@@ -1,4 +1,4 @@
-echo "Welcome to Batch Exeuction"
+echo "*** Welcome to Batch Exeuction ***"
 d:
 cd D:\ITA\ITAutomation_Nov_21_Batch\Eclipse_WS\actitime
 set path=%path%;D:\maven\apache-maven-3.6.1\bin
